8f2f5d57c16a8bcd9317668d01ce5687e7d8ff32
zezecodes <djangsiness01@gmail.com>
2022-02-06 00:23:37 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 print("Welcome to the water remainder system!")
 print("Please follow the instructions to successfully run the program.")
-question1 = input("How many bottles do you normally put in the refridgerator? - ")
+
+question1 = input("How many bottles do you normally put in the refrigerator? - ")
 questionSplit = question1.split()
 questionInt = int(''.join(map(str, questionSplit)))
 remainder = round((questionInt / 2))
@@ -14,9 +15,9 @@ timeTakeout = input("How many times do you take water out of the fridge daily? -
 timeTakeoutSplit = timeTakeout.split()
 timeTakeoutInt = int(''.join(map(str, timeTakeoutSplit)))
 
+reminderTime = round((timeRefillInt * questionInt) / timeTakeoutInt) 
 
-
-while questionInt != remainder:
+while reminderTime > 0 and questionInt != remainder:
     if referenceValue <= remainder :
         print("Please consider refilling your refridgerator with water bottle \n Thank you!")
         break
